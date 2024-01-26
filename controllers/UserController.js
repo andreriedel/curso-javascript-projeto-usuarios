b0dc94cdd2a223b0ce450440a3ec61acbc6c745e
André Riedel <andreriedelz@gmail.com>
2024-01-25 22:00:17 -0300

    feat(photo): get user photo

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -10,7 +10,16 @@ class UserController {
     this.formEl.addEventListener("submit", (event) => {
       event.preventDefault();
 
-      this.addLine(this.getValues());
+      let values = this.getValues();
+
+      this.getPhoto()
+      .then((content) => {
+        values.photo = content;
+        this.addLine(values);
+      })
+      .catch((event) => {
+        console.error(event);
+      });
     });
   }
 
@@ -18,10 +27,12 @@ class UserController {
     let user = {};
 
     [...this.formEl.elements].forEach((field) => {
-      if (field.name == "gender") {
+      if (field.name === "gender") {
         if (field.checked) {
           user[field.name] = field.value;
         }
+      } else if (field.name === "admin") {
+        user[field.name] = field.checked;
       } else {
         user[field.name] = field.value;
       }
@@ -39,14 +50,36 @@ class UserController {
     );
   }
 
+  getPhoto() {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+
+      const file = [...this.formEl.elements].filter((item) => item.name === "photo")[0].files[0];
+  
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (event) => {
+        reject(event);
+      };
+  
+      if (file) {
+        fileReader.readAsDataURL(file);
+      } else {
+        resolve("imgs/defaultuser.jpg");
+      }
+    });
+  }
+
   addLine(user) {
     let tr = document.createElement("tr");
 
     tr.innerHTML = `
-      <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+      <td><img src="${user.photo}" alt="User Image" class="img-circle img-sm"></td>
       <td>${user.name}</td>
       <td>${user.email}</td>
-      <td>${user.admin}</td>
+      <td>${(user.admin) ? "Sim" : "Não"}</td>
       <td>${user.birth}</td>
       <td>
         <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
